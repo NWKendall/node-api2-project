65b300dc562933c833e85a88d92b0b0a66ed229b
nicbongo <nicbongo@protonmail.com>
2020-02-11 17:25:41 -0400

    1 down

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
-
+const postsRouter = require("./data/posts-router");
 const server = express();
 
-const port = 5000;
-server.listen(port, () => console.log(`SERVER WORKING ON PORT: ${port}`))
+server.use(express.json());
+server.use("/api/posts", postsRouter);
+
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Day 2 with Node</h>
+    <p>using backend routers</p>
+  `);
+});
+
+
+
+server.listen(5000, () => console.log(`SERVER WORKING ON PORT: 5000`))
